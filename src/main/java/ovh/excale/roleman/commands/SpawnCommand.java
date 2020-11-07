6ea31c459220b5d421aa3b69dedb1b75307998fe
Excale <dev_excale@hotmail.com>
2020-11-07 22:46:59 +0100

    Created SpawnCommand

---
+++ src/main/java/ovh/excale/roleman/commands/SpawnCommand.java
@@ -0,0 +1,67 @@
+package ovh.excale.roleman.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SpawnCommand extends Command {
+
+	public SpawnCommand() {
+		this.name = "spawn";
+		this.arguments = "<@RoleMention>...";
+		this.help = "Spawns one or more messages to which you can react to, it'll add or remove a certain role.";
+		this.guildOnly = true;
+		this.usesTopicTags = false;
+		this.botPermissions = new Permission[] {
+				Permission.MANAGE_ROLES,
+				Permission.MESSAGE_WRITE,
+				Permission.MESSAGE_MANAGE,
+				Permission.MESSAGE_ADD_REACTION,
+		};
+		this.userPermissions = new Permission[] { Permission.MANAGE_ROLES, Permission.MESSAGE_ADD_REACTION };
+	}
+
+	@Override
+	protected void execute(CommandEvent commandEvent) {
+		Message message = commandEvent.getMessage();
+		MessageChannel channel = message.getChannel();
+		User author = commandEvent.getAuthor();
+
+		List<Role> roles = message.getMentionedRoles();
+
+		try {
+
+			roles.forEach(role ->
+
+					channel.sendMessage(new EmbedBuilder().setTitle(role.getName())
+							.setDescription("React to this message to self add/remove the " + role.getAsMention() + " role")
+							.build()).queue(roleMessage -> {
+								roleMessage.addReaction("\u2705").queue();
+								roleMessage.addReaction("\u274E").queue();
+							},
+							t -> author.openPrivateChannel()
+									.flatMap(dm -> dm.sendMessage(
+											"There has been an error trying to send messages for roles `" + roles.stream()
+													.map(Role::getName)
+													.collect(Collectors.joining()) + "`\n" + t.getMessage()))
+									.queue()));
+
+		} catch(Exception e) {
+			author.openPrivateChannel()
+					.flatMap(dm -> dm.sendMessage("There has been an error trying to send a message from you command.\n" + e
+							.getMessage()))
+					.queue();
+		}
+
+		message.delete().queue();
+	}
+
+}
